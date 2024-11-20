782c59507be4cfaf5ea83142813c396723bdab5c
Phạm Thành Long <167054102+Phamlong2675@users.noreply.github.com>
2024-11-20 17:32:54 +0700

    Update dictation.py

--- dictation.py
+++ dictation.py
@@ -1,6 +1,7 @@
 from nicegui import ui
 import pandas as pd
 import string
+
 class Dictation:
     def __init__(self):
         self.index = 0
@@ -17,6 +18,7 @@ class Dictation:
             self.topic_column = ui.card().classes('w-1/2 max-w-screen-xl mx-auto items-center p-4 gap-6')
             self.dictation_column = ui.card().classes('w-1/2 max-w-screen-xl mx-auto items-center p-4 gap-6')
         self.render_difficulty_page()  # Khởi động ứng dụng tại trang chọn độ khó
+
     def update_audio_file(self):
         audio_file = self.data.iloc[self.index]['audio_file']
         
@@ -25,16 +27,20 @@ class Dictation:
         # Tạo đối tượng âm thanh mới và hiển thị thanh điều khiển
         self.audio_player = ui.audio(audio_file).props('controls').style('width: 300px;')  
         self.audio_player.play()  # Phát âm thanh
+
     def normalize(self, text):
         return str(text).lower().translate(str.maketrans('', '', string.punctuation.replace("'", "")))
+    
     def check_answer(self):
         user_words = self.normalize(self.user_answer).strip().split()
         correct_answer = self.data.iloc[self.index]['sentence']
         correct_words = self.normalize(correct_answer).strip().split()
         return user_words == correct_words
+    
     def play_sound(self, audio_file):
         self.play = ui.audio(audio_file).style('display: none;')  
         self.play.play()
+
     def check_answer_click(self):
         if self.user_answer:
             result = self.check_answer()
@@ -49,10 +55,12 @@ class Dictation:
         else:
             self.notification_label.text = 'Please enter your answer before checking.'
             self.notification_label.style('color: orange;')
+
     def show_answer(self):
         correct_answer = self.data.iloc[self.index]['sentence']
         self.notification_label.text = f"Answer: '{correct_answer}'"
-        self.notification_label.style('color: black;')
+        self.notification_label.style('color: green;')
+
     def skip(self):
         if len(self.data) > 0:  # Kiểm tra xem có dữ liệu trong DataFrame không
             self.index = (self.index + 1) % len(self.data)  # Tăng chỉ số và quay lại đầu nếu vượt quá
@@ -62,6 +70,7 @@ class Dictation:
             self.update_audio_file()  # Cập nhật âm thanh cho câu tiếp theo
             self.no_sens.delete()
             self.no_sens = ui.label(f'({self.index+1}/10)').style('font-size: 18px;')
+
     def turnback(self):
         if len(self.data) > 0:  # Kiểm tra xem có dữ liệu trong DataFrame không
             if self.index > 0:
@@ -74,10 +83,45 @@ class Dictation:
             self.update_audio_file()  # Cập nhật âm thanh cho câu tiếp theo
             self.no_sens.delete()
             self.no_sens = ui.label(f'({self.index+1}/10)').style('font-size: 18px;')
+
     def load_data(self, url):
         self.data = pd.read_csv(url)
         self.index = 0  # Đặt lại chỉ số khi tải dữ liệu mới
         self.update_audio_file()  # Cập nhật tệp âm thanh cho mục đầu tiên
+
+    def go_to_topic_selection(self, difficulty):
+        self.selected_difficulty = difficulty
+        self.render_topic_page()  # Hiển thị trang chọn chủ đề
+
+    def set_topic(self, topic):
+        self.selected_topic = topic
+        self.start_dictation()
+
+    def start_dictation(self):
+        if self.selected_difficulty == 'Easy':
+            if self.selected_topic == 'Movies - Easy':
+                self.load_data('https://raw.githubusercontent.com/Phamlong2675/Python-Project/main/Audio/topic1easy.csv')
+            elif self.selected_topic == 'Snowfall - Easy':
+                self.load_data('https://raw.githubusercontent.com/Phamlong2675/Python-Project/main/Audio/topic2easy.csv')
+            elif self.selected_topic == 'Grant - Easy':
+                self.load_data('https://raw.githubusercontent.com/Phamlong2675/Python-Project/main/Audio/topic3easy.csv')
+            elif self.selected_topic == 'Weather - Easy':
+                self.load_data('https://raw.githubusercontent.com/Phamlong2675/Python-Project/main/Audio/topic4easy.csv')
+            elif self.selected_topic == 'Daily - Easy':
+                self.load_data('https://raw.githubusercontent.com/Phamlong2675/Python-Project/main/Audio/topic5easy.csv')
+        elif self.selected_difficulty == 'Hard':
+            if self.selected_topic == 'Excursion - Hard':
+                self.load_data('https://raw.githubusercontent.com/Phamlong2675/Python-Project/main/Audio/topic1hard.csv')
+            elif self.selected_topic == 'Healthy - Hard':
+                self.load_data('https://raw.githubusercontent.com/Phamlong2675/Python-Project/main/Audio/topic2hard.csv')
+            elif self.selected_topic == 'Restaurant - Hard':
+                self.load_data('https://raw.githubusercontent.com/Phamlong2675/Python-Project/main/Audio/topic3hard.csv')
+            elif self.selected_topic == 'Painting - Hard':
+                self.load_data('https://raw.githubusercontent.com/Phamlong2675/Python-Project/main/Audio/topic4hard.csv')
+            elif self.selected_topic == 'Project - Hard':
+                self.load_data('https://raw.githubusercontent.com/Phamlong2675/Python-Project/main/Audio/topic5hard.csv')
+        self.render_dictation_page()  # Hiển thị trang dictation
+
     def render_difficulty_page(self):
             self.difficulty_column.clear()  # Xóa nội dung cũ
             with self.difficulty_column:
@@ -93,9 +137,7 @@ class Dictation:
             self.difficulty_column.visible = True
             self.topic_column.visible = False
             self.dictation_column.visible = False
-    def go_to_topic_selection(self, difficulty):
-        self.selected_difficulty = difficulty
-        self.render_topic_page()  # Hiển thị trang chọn chủ đề
+
     def render_topic_page(self):
         self.topic_column.clear()  # Xóa nội dung cũ
         with self.topic_column:
@@ -107,50 +149,24 @@ class Dictation:
             # Tạo hàng cho các nút chủ đề
             with ui.row().style('justify-content: center; margin: 10px 0;gap: 10px; flex-wrap: wrap;'):
                     if self.selected_difficulty == 'Easy':
-                        ui.button('Topic 1', on_click=lambda: self.set_topic('Topic 1 - Easy')).props('rounded').classes('w-full bg-indigo hover:bg-indigo text-white font-semibold py-2 rounded-lg shadow-md') 
-                        ui.button('Topic 2', on_click=lambda: self.set_topic('Topic 2 - Easy')).props('rounded').classes('w-full bg-indigo hover:bg-indigo text-white font-semibold py-2 rounded-lg shadow-md')
-                        ui.button('Topic 3', on_click=lambda: self.set_topic('Topic 3 - Easy')).props('rounded').classes('w-full bg-indigo hover:bg-indigo text-white font-semibold py-2 rounded-lg shadow-md')
-                        ui.button('Topic 4', on_click=lambda: self.set_topic('Topic 4 - Easy')).props('rounded').classes('w-full bg-indigo hover:bg-indigo text-white font-semibold py-2 rounded-lg shadow-md')  
-                        ui.button('Topic 5', on_click=lambda: self.set_topic('Topic 5 - Easy')).props('rounded').classes('w-full bg-indigo hover:bg-indigo text-white font-semibold py-2 rounded-lg shadow-md')
+                        ui.button('Movies', on_click=lambda: self.set_topic('Movies - Easy')).props('rounded').classes('w-full bg-indigo hover:bg-indigo text-white font-semibold py-2 rounded-lg shadow-md') 
+                        ui.button('Snowfall', on_click=lambda: self.set_topic('Snowfall - Easy')).props('rounded').classes('w-full bg-indigo hover:bg-indigo text-white font-semibold py-2 rounded-lg shadow-md')
+                        ui.button('Grant', on_click=lambda: self.set_topic('Grant - Easy')).props('rounded').classes('w-full bg-indigo hover:bg-indigo text-white font-semibold py-2 rounded-lg shadow-md')
+                        ui.button('Weather', on_click=lambda: self.set_topic('Weather - Easy')).props('rounded').classes('w-full bg-indigo hover:bg-indigo text-white font-semibold py-2 rounded-lg shadow-md')  
+                        ui.button('Daily', on_click=lambda: self.set_topic('Daily - Easy')).props('rounded').classes('w-full bg-indigo hover:bg-indigo text-white font-semibold py-2 rounded-lg shadow-md')
                     elif self.selected_difficulty == 'Hard':
-                        ui.button('Topic 1', on_click=lambda: self.set_topic('Topic 1 - Hard')).props('rounded').classes('w-full bg-indigo hover:bg-indigo text-white font-semibold py-2 rounded-lg shadow-md')
-                        ui.button('Topic 2', on_click=lambda: self.set_topic('Topic 2 - Hard')).props('rounded').classes('w-full bg-indigo hover:bg-indigo text-white font-semibold py-2 rounded-lg shadow-md')
-                        ui.button('Topic 3', on_click=lambda: self.set_topic('Topic 3 - Hard')).props('rounded').classes('w-full bg-indigo hover:bg-indigo text-white font-semibold py-2 rounded-lg shadow-md')
-                        ui.button('Topic 4', on_click=lambda: self.set_topic('Topic 4 - Hard')).props('rounded').classes('w-full bg-indigo hover:bg-indigo text-white font-semibold py-2 rounded-lg shadow-md')
-                        ui.button('Topic 5', on_click=lambda: self.set_topic('Topic 5 - Hard')).props('rounded').classes('w-full bg-indigo hover:bg-indigo text-white font-semibold py-2 rounded-lg shadow-md')        
+                        ui.button('Excursion', on_click=lambda: self.set_topic('Excursion - Hard')).props('rounded').classes('w-full bg-indigo hover:bg-indigo text-white font-semibold py-2 rounded-lg shadow-md')
+                        ui.button('Healthy', on_click=lambda: self.set_topic('Healthy - Hard')).props('rounded').classes('w-full bg-indigo hover:bg-indigo text-white font-semibold py-2 rounded-lg shadow-md')
+                        ui.button('Restaurant', on_click=lambda: self.set_topic('Restaurant - Hard')).props('rounded').classes('w-full bg-indigo hover:bg-indigo text-white font-semibold py-2 rounded-lg shadow-md')
+                        ui.button('Painting', on_click=lambda: self.set_topic('Painting - Hard')).props('rounded').classes('w-full bg-indigo hover:bg-indigo text-white font-semibold py-2 rounded-lg shadow-md')
+                        ui.button('Project', on_click=lambda: self.set_topic('Project - Hard')).props('rounded').classes('w-full bg-indigo hover:bg-indigo text-white font-semibold py-2 rounded-lg shadow-md')        
             
             # Nút Quay lại căn trái
             ui.button('Back', on_click=self.render_difficulty_page).props('rounded').style('margin: 10px 0; padding: 15px; font-size: 16px; align-self: flex-start;').classes('bg-indigo text-white')
         self.difficulty_column.visible = False
         self.dictation_column.visible = False
         self.topic_column.visible = True
-    def set_topic(self, topic):
-        self.selected_topic = topic
-        self.start_dictation()
-    def start_dictation(self):
-        if self.selected_difficulty == 'Easy':
-            if self.selected_topic == 'Topic 1 - Easy':
-                self.load_data('https://raw.githubusercontent.com/Phamlong2675/Python-Project/main/Audio/topic1easy.csv')
-            elif self.selected_topic == 'Topic 2 - Easy':
-                self.load_data('https://raw.githubusercontent.com/Phamlong2675/Python-Project/main/Audio/topic2easy.csv')
-            elif self.selected_topic == 'Topic 3 - Easy':
-                self.load_data('https://raw.githubusercontent.com/Phamlong2675/Python-Project/main/Audio/topic3easy.csv')
-            elif self.selected_topic == 'Topic 4 - Easy':
-                self.load_data('https://raw.githubusercontent.com/Phamlong2675/Python-Project/main/Audio/topic4easy.csv')
-            elif self.selected_topic == 'Topic 5 - Easy':
-                self.load_data('https://raw.githubusercontent.com/Phamlong2675/Python-Project/main/Audio/topic5easy.csv')
-        elif self.selected_difficulty == 'Hard':
-            if self.selected_topic == 'Topic 1 - Hard':
-                self.load_data('https://raw.githubusercontent.com/Phamlong2675/Python-Project/main/Audio/topic1hard.csv')
-            elif self.selected_topic == 'Topic 2 - Hard':
-                self.load_data('https://raw.githubusercontent.com/Phamlong2675/Python-Project/main/Audio/topic2hard.csv')
-            elif self.selected_topic == 'Topic 3 - Hard':
-                self.load_data('https://raw.githubusercontent.com/Phamlong2675/Python-Project/main/Audio/topic3hard.csv')
-            elif self.selected_topic == 'Topic 4 - Hard':
-                self.load_data('https://raw.githubusercontent.com/Phamlong2675/Python-Project/main/Audio/topic4hard.csv')
-            elif self.selected_topic == 'Topic 5 - Hard':
-                self.load_data('https://raw.githubusercontent.com/Phamlong2675/Python-Project/main/Audio/topic5hard.csv')
-        self.render_dictation_page()  # Hiển thị trang dictation
+
     def render_dictation_page(self):
         self.dictation_column.clear()  # Xóa nội dung cũ
         with self.dictation_column:
